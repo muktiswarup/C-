ff506b5c782a4acee193359524158137d94d6707
muktiswarup <csjena1999@gmail.com>
2024-08-17 12:05:59 +0530

    j

--- Loop/loop1.cpp
+++ Loop/loop1.cpp
@@ -12,7 +12,7 @@ int main(){
 
 } */
 
-
+//.......................................................
 // W.A.P. to add all the nth element;
 /* #include <iostream>
 using namespace std;
@@ -28,7 +28,7 @@ int main(){
     }
     cout<< sum;
 } */
-
+//.........................................................
 // wap to print the prime number..
 /* #include<iostream>
 using namespace std;
@@ -48,7 +48,7 @@ int main(){
         i=i+1;
     }
 } */
-
+/........................................................................
 // wap to add all the even number..
 /* #include<iostream>
 using namespace std;
@@ -66,21 +66,122 @@ int main(){
 
 
 } */
-
-#include<iostream>
+//........................................................
+/* #include<iostream>
 using namespace std;
 int main(){
-int n;
-cout << " enter a no." <<endl;
-cin>> n;
-int i=2;
-while(i<n){
-    if(n%i==0){
-        cout<< "not a prime no." << i<< endl;
+    int n;
+    cout<< " Enter The number:";
+    cin>> n;
+    int i=2;
+    while(i<n){
+        if(n%i==0){
+            cout<< "It's not a prime number.";
+        }else{
+            cout<< "it's a prime number.";
+        }
+        i=i+1;
     }
-    else{
-          cout<< "prime no."<< i <<endl;
+} */
+
+//........................................................
+/*
+            1 2 3 4 5 
+            1 2 3 4 5 
+            1 2 3 4 5 
+            1 2 3 4 5 
+            1 2 3 4 5 
+
+ #include<iostream>
+using namespace std;
+int main(){
+    int n ;
+    cout<< "enter a number" << endl;
+    cin>> n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<< j << " " ;
+            j=j+1;
+        }
+        cout<< endl;
+        i=i+1;
     }
-    i=i+1;
-}
+} */
+//.......................................................................
+/*      
+            * * * * * 
+            * * * * 
+            * * * 
+            * * 
+            * 
+ #include<iostream>
+using namespace std;
+int main (){
+    int n ;
+    cout << "enter a number" << endl;
+    cin>> n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            if(i<=j){
+                cout<< "*" << " " ;
+            }
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+} */
+
+//...........................................................
+
+/* 
+
+            *
+            * *
+            * * *
+            * * * * 
+            * * * * *
+ #include<iostream>
+using namespace std;
+int main (){
+    int n ;
+    cout << "enter a number" << endl;
+    cin>> n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            if(j<=i){
+                cout<< "*" << " " ;
+            }
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
+} */
+
+
+ #include<iostream>
+using namespace std;
+int main (){
+    int n ;
+    cout << "enter a number" << endl;
+    cin>> n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            if(i<=j){
+                cout<< "*" << " " ;
+            }
+            j=j+1;
+        }
+        cout<<endl;
+        i=i+1;
+    }
 }
