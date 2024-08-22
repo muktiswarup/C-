e2d0598bfba77d6d610869672c24de495114154c
muktiswarup <csjena1999@gmail.com>
2024-08-22 20:41:18 +0530

    'j'

--- Loop/loop2.cpp
+++ Loop/loop2.cpp
@@ -170,3 +170,21 @@ int main(){
         i=i+1;
     }
 } */
+
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << " Enter the number ";
+    cin >> n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            if(i==1 || i== n || j==1 j==n){
+                cout << " *";
+            }
+            j=
+        }
+    }
+} 
