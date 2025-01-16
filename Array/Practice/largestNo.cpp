962fb187a08874e4a94522f826267318a40f549b
muktiswarup <csjena1999@gmail.com>
2025-01-16 16:09:19 +0530

    reference variable

--- Array/Practice/largestNo.cpp
+++ Array/Practice/largestNo.cpp
@@ -20,9 +20,28 @@ int secondLargest(int arr[],int n){
     }
     return secondMax;
 }
+int  sorted (int arr[],int n ){
+    int no=0;
+    int b=arr[0];
+    for (int i =1;i<n ;i++){
+        if (arr[b]>arr[i]){
+            no++;
+            b++;
+        }
+    }
+    return no ;
+}
 int main (){
-    int arr [5]= {52,14,85,36,14};
+    int arr [5]= {1,2,3,4,5};
     int n =5;
     cout << "The largest element of the array is : " << largest(arr,n) << endl;
-    cout << "The second largest element of the array is : " << secondLargest(arr,n);
+    cout << "The second largest element of the array is : " << secondLargest(arr,n)<<endl;
+    
+    int check = sorted(arr,n);
+    cout << "the no is " << check << endl;
+    if (check==0){
+        cout <<"its a sorted array" << endl; 
+    }else {
+        cout << "ITS not a sorted array " << endl;
+    }
 }
---
+++ Pointer/ReferenceVariable.cpp
@@ -0,0 +1,73 @@
+// // Same memory but differnt name 
+// #include<bits/stdc++.h>
+// using namespace std ;
+// void update (int &n ){
+//     n++;
+// }
+
+// int main (){
+//     int i =5;
+//     // int &j =i;
+//     // cout << i << endl;
+//     // cout << j << endl;
+//     // i++;
+//     // cout << j << endl; 
+//     // j++;
+//     // cout << i << endl ;
+//     // update(i) ;
+//     // cout << i << endl;
+//     char *ch = new char ;
+//     cout << ch << endl;
+//     cout << *ch << endl;
+//     cout << sizeof(new char) << endl;
+//     cout << sizeof(new int ) << endl;
+//     cout << sizeof(ch) << endl;
+
+// }
+
+
+/* #include<bits/stdc++.h>
+using namespace std ;
+
+int sum(int *arr, int n){
+    int sum =0;
+    for (int i =0;i<n ;i++){
+        sum = sum + arr[i];
+    }
+    return sum ;
+}
+
+int main (){
+    cout << "Enter the number of element in the array " << endl;
+    int n ;
+    cin >> n ;
+    int *arr = new int[n];
+    cout << "Enter the value of the array " << endl;
+    for (int i =0;i<n ;i++){
+        cin >> arr[i] ;
+    }
+    cout << "The sum of the arrray is " <<sum(arr,n);
+
+} */
+
+
+#include<bits/stdc++.h>
+using namespace std ;
+int update (int *arr,int n ){
+    int sum =0;
+    for (int i =0;i<n ;i++){
+        sum = sum + arr[i];
+    }
+    return sum ;
+}
+int main (){
+cout << "Enter the number of the array elemnt you want to enter " << endl;
+int n ;
+cin >> n ;
+int *arr =new int[n];
+cout << "Enter the value of the array " << endl;
+for (int i =0;i<n;i++){
+    cin >> arr[i];
+}
+cout << "The sum of the array is " << update(arr,n) << endl;
+}
